9900d8bca8ce03e7df806b3ef7cb52401730fed6
Orest Mahdziack <orestit@icloud.com>
2023-01-24 00:22:04 +0200

    BRG: Orest test commit 2

--- src/main/java/com/hoverla/bring/Hello.java
+++ src/main/java/com/hoverla/bring/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Sonar test");
+        System.out.println("Sonar 2");
     }
 }
