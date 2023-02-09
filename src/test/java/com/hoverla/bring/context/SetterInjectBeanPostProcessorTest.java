77013c569d63edc2d1656b2ffd273d366399fd46
tshurhot <tarasuchok@ukr.net>
2023-02-09 13:55:28 +0200

    refactoring

--- src/test/java/com/hoverla/bring/context/SetterInjectBeanPostProcessorTest.java
+++ src/test/java/com/hoverla/bring/context/SetterInjectBeanPostProcessorTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SetterAutowiringPostProcessorTest {
+public class SetterInjectBeanPostProcessorTest {
 
     @Test
     @Order(1)
